835a85264bf671006533fa3579e3fc49f78bebd9
OlegAba <olegaba.developer@gmail.com>
2020-11-07 18:23:39 -0500

    Updates

---
+++ precache-manifest.388e65a9177478aaa26b6ade6a85d1d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d50b7abc97fed40e41240cb8af75e6",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "cb00f94aba8d3c4a587e",
+    "url": "/personal-portfolio/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "6ced25a6e6dbd2e3b222",
+    "url": "/personal-portfolio/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "cb00f94aba8d3c4a587e",
+    "url": "/personal-portfolio/static/js/2.946a301d.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/personal-portfolio/static/js/2.946a301d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ced25a6e6dbd2e3b222",
+    "url": "/personal-portfolio/static/js/main.a319b6c4.chunk.js"
+  },
+  {
+    "revision": "1fd8320ed472344e7a3b",
+    "url": "/personal-portfolio/static/js/runtime-main.41dc66a3.js"
+  }
+]);
