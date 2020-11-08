eeacbdf8bdd579b84f33b12c69e3d820e6705681
OlegAba <olegaba.developer@gmail.com>
2020-11-07 19:30:23 -0500

    Updates

---
+++ precache-manifest.45fb01feca4c02ea0b5c71bb85e3bb45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a72714afc592d510fdc9cb7eafe28c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11e67141d0fa1e8868b3",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "0f2f8c467fab3b66a2ff",
+    "url": "/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "11e67141d0fa1e8868b3",
+    "url": "/static/js/2.e00e628c.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/static/js/2.e00e628c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2f8c467fab3b66a2ff",
+    "url": "/static/js/main.95e8fa67.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  }
+]);
